f39a7181d3ac5919848d48223cc00ec46a2808b9
starybog <nikitapishchalin@gmail.com>
2024-10-07 19:39:44 +0300

    main.py

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-#вводим данные с клавиатуры(день и месяц) 
-day = int(input("введите день: "))
+#вводим данные с клавиатуры(дату и месяц) 
+day = int(input("введите дату: "))
 month = int(input("введите месяц: "))
 #используем операторы ветвления if elif else для определение сезона и выводим значения 
-if month >= 3 and month <= 5:
+if 1 <= day <= 31 and month == 3 or 1 <= day <= 30 and month == 4 or 1 <= day <= 31 and month == 5: #проверяем введенные данные для весны
     print("весна")
-elif month >= 6 and month <= 8:
+elif 1 <= day <= 30 and month == 6 or 1 <= day <= 31 and month == 7 or 1 <= day <= 31 and month == 7: #проверяем введенные данные для лето
     print ("лето")
-elif month >= 9 and month <=11:
+elif 1 <= day <= 30 and month == 9 or 1 <= day <= 31 and month == 10 or 1 <= day <= 30 and month == 11: #проверяем введенные данные для осени
     print ("осень")
-elif month == 12 or month <=3:
-    print ("зима")
-    #если ввели не существующий месяц то error
+elif 1 <= day <= 31 and month == 12 or 31 >= day >= 1 and month == 1 or 1 <= day <= 28 and month == 2: #проверяем введенные данные для зимы
+    print ("зима") 
+    #если ввели не существующий месяц или дату выводим error
 else: print("error")
+
